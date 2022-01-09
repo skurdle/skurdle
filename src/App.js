e78e5671899d9c87bbbb770c82e1cfcba8d1befc
Uzair Shamim <uzashamim@gmail.com>
2022-01-09 14:00:37 -0500

    Initialize the app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { default as WordDisplay } from "./components/WordDisplay.js";
+
+function App() {
+    return (
+        <div className="App">
+            <WordDisplay />
+        </div>
+    );
+}
+
+export default App;
